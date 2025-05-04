b69ee3bfe58bac5547cdc965ae79cf4b927a6592
Forever Park <140984288+foreverpark@users.noreply.github.com>
2025-05-03 19:54:43 -0600

    Basic Camera Movement

---
+++ Ranguito/Assets/Scripts/Jon/CameraMovement.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public PlayerController player;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var playerPosition = player.transform.position;
+        UpdateCameraPosition(playerPosition);
+    }
+
+    public void UpdateCameraPosition(Vector3 playerPosition)
+    {
+        if (playerPosition.x != transform.position.x || playerPosition.y != transform.position.y)
+        {
+            transform.position = new Vector3(playerPosition.x, playerPosition.y, transform.position.z);
+        }
+    }
+}
