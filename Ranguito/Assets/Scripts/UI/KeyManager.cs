d148fbb28586682ec12c4c6c70cb87f5029d11d7
Forever Park <140984288+foreverpark@users.noreply.github.com>
2025-06-08 23:43:26 -0600

    Key Get Sound

--- Ranguito/Assets/Scripts/UI/KeyManager.cs
+++ Ranguito/Assets/Scripts/UI/KeyManager.cs
@@ -5,9 +5,11 @@ public class KeyManager : MonoBehaviour
     public GameObject image;
     public GameObject image2;
     public KeyDoor keyDoor;
+    private AudioController audioController;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void Start()
     {
+        audioController = GetComponent<AudioController>();
         if (ScoringManager.gotGreat)
         {
             image2.SetActive(true);
@@ -15,12 +17,14 @@ public class KeyManager : MonoBehaviour
     }
     public void GetKey()
     {
+        audioController.PlayAudio(0);
         image.SetActive(true);
         keyDoor.canOpen = true;
     }
 
     public void GetGreat()
     {
+        audioController.PlayAudio(0);
         image2.SetActive(true);
     }
 
